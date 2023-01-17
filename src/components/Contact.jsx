4b308ebcf12ad9e0d73861566fb948e7a54ecee7
austine5n6 <austinesofine@yahoo.com>
2023-01-17 12:29:58 +0000

    fully resolved contact-form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,33 +1,15 @@
 import React, { useState } from 'react'
 
 const Contact = () => {
+    const [form_name, setName] = useState('')
+    const [form_email, setEmail] = useState('')
+    const [form_message, setMessage] = useState('')
 
-    const [name, setName] = useState("");
-    const [names, setNames] = useState([]);
-    const [email, setEmail] = useState("")
-    const [emails, setEmails] = useState([])
-    const [message, setMessage] = useState("")
-    const [messages, setMessages] = useState([])
-
-    const handleChange = (e) => {
-        setName(e.target.value);
-        setEmail(e.target.value);
-        setMessage(e.target.value);
-            
-    }
     const handleClick = () => {
-       setNames([...names, name])
-       setEmails([...emails, email])
-        setMessages([...messages, message])
-        setName("")
-        setEmail("")
-        setMessage("")
-
+        setName('')
+        setEmail('')
+        setMessage('')
     }
-
-        //  refreshPage = ()=> {
-        //     window.location.reload();
-        //  }
 
   return (
   <div name="contact" className="w-full h-screen bg-gradient-to-b
@@ -43,26 +25,32 @@ const Contact = () => {
                  <input 
                  type="text" 
                  name="name" 
+                 value ={form_name}
+                 onChange={(e) => setName(e.target.value)}
                  placeholder="Enter your name" 
                  required
                  className="p-2 bg-transparent border-2 rounded-md text-white 
                  focus:outline-none"
-                 onChange={(e) => handleChange(e)} 
+                  
                  />
                   <input 
                  type="text" 
                  name="email"
+                 value={form_email}
+                 onChange={(e) => setEmail(e.target.value)}
                  required 
                  placeholder="Enter your email" 
                  className="my-4 p-2 bg-transparent border-2 rounded-md text-white 
                  focus:outline-none" 
-                 onChange={(e) => handleChange(e)}
+                 
                  />
-                 <textarea name="message" rows="10" className="p-2 bg-transparent border-2 rounded-md 
-                 text-white focus:outline-none"
-                 onChange={(e) => handleChange(e)}>
+                 <textarea name="message" rows="10" 
+                 className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+                 value={form_message}
+                 onChange={(e) => setMessage(e.target.value)}
+                 >
                  </textarea>
-                    <button onClick={() => handleClick()} className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r 
+                    <button onSubmit={() => handleClick()} className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r 
                     from bg-white to to-gray-400 cursor-pointer hover:scale-105">Send
                         </button>
              </form>
